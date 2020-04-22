10a89d7f21f91de02676463cbb2bf84c6f56b1be
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-04-22 20:54:41 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Zajęcia 2020.04.21/3dod.cpp
+++ Zajęcia 2020.04.21/3dod.cpp
@@ -20,11 +20,11 @@ int main()
     l2 *= g / m2;//licznik 2
     l1 += l2;//suma liczników
     l2 = gcd(l1, g);//gcd licznika i mianownika
-    l1 /= l2;
-    g /= l2;
-    if(l1 / g)
+    l1 /= l2;//podziel mianownik przez gcd
+    g /= l2;//podziel mianownik przez gcd
+    if(l1 / g)//jeśli jest liczba całkowita
         cout << l1 / g << ' ';
-    if(l1 % g)
+    if(l1 % g)//jeśli jest ułamek
         cout << l1 % g << '/' << g;
     return 0;
 }
