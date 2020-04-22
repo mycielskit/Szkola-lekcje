32b73c4857b5d6e8da10bc5a95eb0c54ee7c7bf5
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-04-22 19:02:25 +0200

    added 3dod.cpp

---
+++ Zajęcia 2020.04.21/3dod.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) { 
+   if (b == 0) 
+      return a; 
+   return gcd(b, a % b);  
+} 
+
+int main()
+{
+   int l1, l2, m1, m2, g;
+   cin >> l1 >> m1 >> l2 >> m2;
+   g = m1 * m2 / gcd(m1, m2);
+   l1 *= g / m1;
+   l2 *= g / m2;
+   l1 += l2;
+   l2 = gcd(l1, g);
+   l1 /= l2;
+   g /= l2;
+   if(l1 / g)
+      cout << l1 / g << ' ';
+   if(l1 % g)
+      cout << l1 % g << '/' << g;
+}
