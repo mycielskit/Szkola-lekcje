b1b1b673c92de4acbbbb6949ab3a5cdc44f66876
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-09-04 12:26:43 +0200

    Create bit.cpp

---
+++ Klasa2/Zajecia-2020.09.04/bit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if(a > 100)
+        a = 100;
+    while(a--)
+        cout << "DZIEN DOBRY\n";
+    return 0;
+}
