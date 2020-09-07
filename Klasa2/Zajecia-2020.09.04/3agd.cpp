d2a85031f0a366b6cec8de173695e38144318e2e
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-09-07 22:16:40 +0200

    Update 3agd.cpp

--- Klasa2/Zajecia-2020.09.04/3agd.cpp
+++ Klasa2/Zajecia-2020.09.04/3agd.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main()
 {
-    float money, time, rate;
+    float money, time;
     cin >> money >> time;
+    money /= time;
     if(time <= 12)
-        rate = 1.025;
+        money *= 1.025;
     else if(time <= 24)
-        rate = 1.05;
+        money *= 1.05;
     else
-        rate = 1.1;
-    printf("%.2f", (money/time)*rate);
+        money *= 1.1;
+    printf("%.2f", money);
     return 0;
 }
