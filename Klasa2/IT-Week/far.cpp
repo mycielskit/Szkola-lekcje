8083b8bacce232005ee4889fa0b506a8fab411a6
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-10-12 13:42:09 +0200

    Update far.cpp

--- Klasa2/IT-Week/far.cpp
+++ Klasa2/IT-Week/far.cpp
@@ -5,25 +5,17 @@ using namespace std;
 int main()
 {
     int n, out = 0; cin >> n;
-    bool a[n * n] = {};
+    int a[n] = {};
     int x1, x2, h;
     while(1)
     {
         cin >> x1 >> x2 >> h;
-        if(x1-- == 0 && x2 == 0 && h == 0)//check for 0 0 0
+        if(!(x1--) && !x2 && !h)//check for 0 0 0
             break;
         for(int i = x1; i < x2; i++)
-        {
-            for(int j = 0; j < h; j++)
-                a[i * n + j] = 1;
-        }
+            a[i] = max(a[i], h);
     }
-    for(int i = n - 1; i >= 0; i--)
-    {
-        for(int j = 0; j < n; j++)
-            //cout << a[j * n + i] << ' ';
-            out += a[j * n + i];
-        //cout << "\b\n";
-    }
+    for(int i = 0; i < n; i++)
+        out += a[i];
     cout << out << endl;
 }
